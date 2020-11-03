21bea2c65f191912ab5939526e1316526ec9a8d3
rhino0419 <rhino0419@gmail.com>
2020-11-03 11:25:43 +0530

    My new commit -x

---
+++ src/test/java/myTest/TestCases.java
@@ -0,0 +1,35 @@
+package myTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestCases {
+
+	SoftAssert softAssert = new SoftAssert();
+	
+	@Test
+	public void test1() {
+		System.out.println("Open Browser");
+		
+		Assert.assertEquals(true, true);//Hard assertion
+		
+		System.out.println("Enter username");
+		System.out.println("Enter Password");
+		System.out.println("Click on sign in button");
+		
+		softAssert.assertEquals(true, false,"Home page title is not correct");//Hard assertion
+		
+		System.out.println("Validate homePage");
+		
+		softAssert.assertEquals(true, false);//Hard assertion
+		
+		System.out.println("Go to deals page");
+		System.out.println("Create a deal");
+		
+		System.out.println("Go to contacts page");
+		System.out.println("Create a contacts");
+		
+		softAssert.assertAll();
+	}
+}
